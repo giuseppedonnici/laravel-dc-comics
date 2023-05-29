f5ff3c5d8783dd5f76bbdf2a8e5cd1070d3b6c43
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-05-29 15:03:21 +0200

    Add: ComicsTableSeeder

--- database/migrations/2023_05_29_120732_create_comics_table.php
+++ database/migrations/2023_05_29_120732_create_comics_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->string('title', 50);
             $table->text('description')->nullable();
             $table->text('image');
-            $table->decimal('price', 5, 2);
-            $table->string('series', 30);
+            $table->string('price', 10);
+            $table->string('series', 50);
             $table->string('sale_date', 10);
             $table->string('type', 30);
             $table->timestamps();
