df410ca28d1727719006b55fc1d355d45b8c4994
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-06-01 14:11:54 +0200

    Add: function messages to StoreComicRequest

--- app/Http/Requests/StoreComicRequest.php
+++ app/Http/Requests/StoreComicRequest.php
@@ -33,4 +33,31 @@ class StoreComicRequest extends FormRequest
             'type' => 'required|min:2|max:30'
         ];
     }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages() {
+        return [
+            'title.required' => 'Il titolo è richiesto e deve essere lungo almeno 5 caratteri',
+            'title.min' => 'Il titolo deve essere lungo almeno 5 caratteri',
+            'title.max' => 'Il titolo non deve superare i 50 caratteri',
+            'description.required' => 'La descrizione è richiesta',
+            'image.required' => "L'immagine è richiesta",
+            'price.require' => 'Il prezzo è richiesto e deve essere di almeno 2 cifre',
+            'price.min' => 'Il prezzo deve essere lungo almeno 2 cifre',
+            'price.max' => 'Il prezzo non deve superare le 10 cifre',
+            'series.required' => 'La serie è richiesta e deve essere lunga almeno 5 caratteri',
+            'series.min' => 'La serie deve essere lunga almeno 5 caratteri',
+            'series.max' => 'La serie non deve superare i 50 caratteri',
+            'sale_date' => 'La data di uscita è richiesta e non deve superare le 10 cifre',
+            'sale_date.max' => 'La data di uscita non deve superare le 10 cifre',
+            'type' => 'La tipologia è richiesta e deve essere lunga almeno 2 caratteri',
+            'type.min' => 'La tipologia deve essere lunga almeno 2 caratteri',
+            'tyoe.max' => 'La tipologia non deve superare i 30 caratteri'
+
+        ];
+    }
 }
