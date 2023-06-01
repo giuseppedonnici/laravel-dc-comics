c7a5f49595bd1b21dffff953ca040407f5de2a7c
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-06-01 14:45:51 +0200

    Add: features on edit view

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -23,42 +23,42 @@
 
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}">
+            <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $comic->title) }}">
         </div>
         
         <div class="mb-3">
             <label for="image" class="form-label">Image</label>
-            <input type="text" class="form-control" id="image" name="image" value="{{ $comic->image }}">
+            <input type="text" class="form-control" id="image" name="image" value="{{ old('image', $comic->image) }}">
         </div>
 
         <div class="mb-3">
             <label for="type" class="form-label">Type</label>
             <select id="type" name="type" class="form-select">
                 <option></option>
-                <option @selected($comic->type === 'comic_book') value="comic_book">comic book</option>
-                <option @selected($comic->type === 'grapgic_novel') value="grapgic_novel">graphic novel</option>
+                <option @selected(old('type', $comic->type) === 'comic book') value="comic_book">comic book</option>
+                <option @selected(old('type', $comic->type) === 'graphic novel') value="grapgic_novel">graphic novel</option>
             </select>
         </div>
 
         <div class="mb-3">
             <label for="series" class="form-label">Series</label>
             <input type="text" class="form-control" id="series" name="series"
-                value="{{ $comic->series }}">
+                value="{{ old('series', $comic->series) }}">
         </div>
 
         <div class="mb-3">
             <label for="price" class="form-label">Price</label>
-            <input type="text" class="form-control" id="price" name="price" value="{{ $comic->price }}">
+            <input type="text" class="form-control" id="price" name="price" value="{{ old('price', $comic->price) }}">
         </div>
 
         <div class="mb-3">
             <label for="sale_date" class="form-label">Sale Date</label>
-            <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}">
+            <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
         </div>
 
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
-            <textarea class="form-control" name="description" id="description" rows="3">{{ $comic->description }}</textarea>
+            <textarea class="form-control" name="description" id="description" rows="3">{{ old('description', $comic->description) }}</textarea>
         </div>
 
         <button class="btn btn-primary" type="submit">Send</button>
